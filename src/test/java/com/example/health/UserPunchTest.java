99eeab24d3ef922f42e57cd657f9f9a095954ac3
noobmantest <noobmantest@163.com>
2022-01-27 17:39:36 +0800

    最后提交，保存提交

--- src/test/java/com/example/health/UserPunchTest.java
+++ src/test/java/com/example/health/UserPunchTest.java
@@ -1,7 +1,9 @@
 package com.example.health;
 
+import com.example.health.config.MyConfig;
 import com.example.health.entity.User;
 import com.example.health.service.UserService;
+import com.example.health.tools.AutoPunchOperate;
 import com.example.health.tools.InterfaceVisitPost;
 import com.example.health.tools.SendEmailToUser;
 import com.example.health.tools.UserPunch;
@@ -9,7 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserPunchTest {
 
@@ -28,13 +32,19 @@ class StudyActuatorApplicationTests {
 
     @Test
     void contextLoads() {
-//        User user = new User("2222", "11111", 30, "0", "1945716435@qq.com", "410100", "测试地址");
-//        sendEmailToUser.sendEmailToUser(user, "test", "test");
+        User user = new User("20177720415", "07024612", 30, "0",
+                "1945716435@qq.com", "410100", "测试地址");
+        Map<String, String> map = new HashMap<>();
+        map.put("user", user.getUser());
+        map.put("password", user.getPassword());
+        map.put("address", user.getAddress());
+        map.put("city_code", user.getCity_code());
 
-//        System.out.println(userService);
-//        User user = new User("2222", "11111", 30, "0", "asdf.qq.com", "410100", "测试地址");
-//        userService.insertUser(user);
-//        System.out.println(userService.findAll());
+        // 访问接口
+        String res = new InterfaceVisitPost().post("http://81.70.250.230:8888/login", map);
+        System.out.println(res);
+
+
     }
 
 }
